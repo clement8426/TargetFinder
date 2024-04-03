ca17dad9eaeea022a2ad31963c1b1e9708b01d9b
clement bastien <bastienclement826@gmail.com>
2024-04-03 11:43:34 +0200

    add start rating

---
+++ app/javascript/controllers/club_show_controller.js
@@ -0,0 +1,35 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="club-show"
+export default class extends Controller {
+  connect() {
+    const stars = this.element.querySelectorAll(".rating-star-label");
+
+    stars.forEach((star, index) => {
+      star.addEventListener("mouseover", function() {
+        stars.forEach((s, i) => {
+          if (i <= index) {
+            s.classList.add("selected");
+          } else {
+            s.classList.remove("selected");
+          }
+        });
+      });
+
+      star.addEventListener("click", function() {
+        stars.forEach((s, i) => {
+          if (i <= index) {
+            s.classList.add("validated");
+          } else {
+            s.classList.remove("validated");
+          }
+        });
+      });
+    });
+  }
+
+  toggleCommentForm() {
+    const commentFormContainer = document.getElementById('comment-form-container');
+    commentFormContainer.classList.toggle('hidden');
+  }
+}
