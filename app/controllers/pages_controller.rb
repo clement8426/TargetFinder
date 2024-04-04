5da9c9402efea9bbd3ab5ecbb85255f58cdf07c3
clement bastien <bastienclement826@gmail.com>
2024-04-04 12:19:58 +0200

    ll

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,17 @@ class PagesController < ApplicationController
         marker_html: render_to_string(partial: "marker")
       }
     end
+    @clubs_with_region = Club.all.map do |club|
+      location = Geocoder.search([club.latitude, club.longitude]).first
+      if location.present? && location.data["address"].present?
+        region_code = location.data["address"]["ISO3166-2-lvl6"] || location.data["address"]["ISO3166-2-lvl6"]
+        region_code_digits = region_code.scan(/\d/).join
+        { club: club, region_code: region_code_digits }
+      end
+    end.compact
+
+    # Trie les clubs par code de région
+    @sorted_clubs = @clubs_with_region.sort_by { |club_info| club_info[:region_code] }
+
   end
 end
